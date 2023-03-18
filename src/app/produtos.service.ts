4bc9ee2b2b4c5c87d278d0756c488b81fc0262d2
vinicius <viniciusblima_22@hotmail.com>
2023-03-18 18:24:38 -0300

    build

---
+++ src/app/produtos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { IProduto, produtos } from './produtos/produtos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutosService {
+produtos: IProduto[] = produtos;
+
+  constructor() { }
+  getall() {
+    return this.produtos;
+  }
+  getOne(produtoId: number){
+    return this.produtos.find(produto =>produto.id == produtoId)
+  }
+}
